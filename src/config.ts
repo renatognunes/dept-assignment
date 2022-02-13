908e9f80b516f9901583a3d6adb6ada338a7f151
Renato G Nunes <renatognunes@Renatos-Macbook-Pro.local>
2022-02-13 01:05:30 -0300

    update mocked JSON storage endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const ENDPOINT_PARTNERS = 'https://www.npoint.io/docs/44d6cf784f879462040d';
+export const ENDPOINT_PARTNERS = 'https://api.npoint.io/44d6cf784f879462040d';
 export const ENDPOINT_CONTENT = 'https://api.npoint.io/8df692f8c9dc9f8a95ff';
